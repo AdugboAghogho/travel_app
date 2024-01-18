b4e349a2f8215a2d6ff26c95cb347937afc05d6c
adugbo aghogho <adugboaghogho@gmail.com>
2024-01-18 13:50:30 +0100

    starting features

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,10 +1,23 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Features = () => {
   return (
-    <div>
-      Features
-    </div>
+    <section className="flex-col flexCenter overflow-hidden bg-feature-bg-bg-center bg-no-repeat py-24">
+      <div className="max-container padding-container relative w-full justify-end">
+        <div className="flex flex-1 lg:min-h-[900px]">
+          <Image 
+            src="/phone.png"
+            alt='phone'
+            width={440}
+            height={1000}
+            className='feature-phone'
+          />
+        </div>
+
+        .
+      </div>
+    </section>
   )
 }
 
