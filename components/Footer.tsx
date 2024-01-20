9b6d581c550b36173334c02f013d2370859c78f3
adugbo aghogho <adugboaghogho@gmail.com>
2024-01-20 18:04:58 +0100

    contact us

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,4 @@
-import { FOOTER_LINKS } from '@/constants'
+import { FOOTER_CONTACT_INFO, FOOTER_LINKS } from '@/constants'
 import { link } from 'fs'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -30,6 +30,25 @@ const Footer = () => {
                 </ul>
               </FooterColumn>
             ))}
+
+            <div className="flex flex-col gap-5">
+              <FooterColumn title={FOOTER_CONTACT_INFO.title}>
+               {FOOTER_CONTACT_INFO.links.map((link) => (
+                  <Link
+                  href='/'
+                  key={link.label}
+                  className='flex gap-4 md:flex-col lg:flex-row'
+                >
+                  <p className="whitespace-nowrap">
+                    {link.label}:
+                  </p>
+                  <p className="medium-14 whitespace-nowrap text-blue-70">
+                    {link.value}
+                  </p>
+                </Link>
+               ))}
+              </FooterColumn>
+            </div>
           </div>
         </div>
       </div>
